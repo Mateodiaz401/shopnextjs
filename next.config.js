cf2505c401c73b1b8d7c45c6c0433231f601df9c
Mateo Díaz <mateodiaz401@gmail.com>
2022-06-13 23:41:06 -0500

    second commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    customKey: 'customValue',
+  },
+  images: {
+    domains: ['placeimg.com', 'api.lorem.space'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/pages/_app.js
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import Script from 'next/script';
+import Header from '@components/Header';
+import AppContext from '@context/AppContext';
+import useInitialState from 'hooks/useInitialState';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  const initialState = useInitialState();
+  return (
+    <AppContext.Provider value={initialState}>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-LM50EEDM7G" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-LM50EEDM7G');`}
+      </Script>
+      <Head>
+        <title>Nextjs Shop</title>
+      </Head>
+      <Header></Header>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
+}
+
+export default MyApp;
